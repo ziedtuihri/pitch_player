f00798289f38a5e894ead9dc97c22f4ed3a1add2
zied <zied.hack@gmail.com>
2023-05-15 21:09:06 +0200

    init commit

---
+++ src/pitch_player/Pitch_player.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pitch_player;
+
+/**
+ *
+ * @author ztouahri2
+ */
+public class Pitch_player {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
