1dac3d104553183cde5c4493eb0842974cd8cc6f
zied <zied.hack@gmail.com>
2023-05-17 14:35:36 +0200

    equipe

--- src/edu/connection1cinfo1/entities/Equipe.java
+++ src/edu/connection1cinfo1/entities/Equipe.java
@@ -7,7 +7,7 @@ package edu.connection1cinfo1.entities;
 
 /**
  *
- * @author WIJDEN
+ * @author Zied
  */
 public class Equipe {
 
--- src/edu/connection1cinfo1/services/EquipeCRUD.java
+++ src/edu/connection1cinfo1/services/EquipeCRUD.java
@@ -9,12 +9,15 @@ import edu.connection1cinfo1.entities.Equipe;
 import edu.connection1cinfo1.interfaces.ICRUD;
 import edu.connection1cinfo1.utils.MyConnection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author WIJDEN
+ * @author Zied
  */
 public class EquipeCRUD implements ICRUD<Equipe>{
 
@@ -38,7 +41,26 @@ public class EquipeCRUD implements ICRUD<Equipe>{
 
     @Override
     public List<Equipe> displayEntities() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Equipe> myList = new ArrayList<>();
+        try{
+            String requet = "SELECT * FROM equipe";
+            Statement st = MyConnection
+                    .getInstance()
+                    .getCnx()
+                    .createStatement();
+            ResultSet rs = st.executeQuery(requet);
+            while(rs.next()){
+                Equipe p = new Equipe();
+                p.setId(rs.getInt(1));
+                p.setNom(rs.getString("nom_equipe"));
+                p.setAnneeFondation(rs.getInt("annee_fondation"));
+                myList.add(p);
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    return myList;
     }
 
     @Override
@@ -48,12 +70,36 @@ public class EquipeCRUD implements ICRUD<Equipe>{
 
     @Override
     public void update(Equipe entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String requet = "UPDATE equipe SET nom_equipe= ?, annee_fondation= ? WHERE id_equipe = ?";
+        try (PreparedStatement pst = MyConnection.getInstance().getCnx().prepareStatement(requet)) {
+            pst.setString(1, entity.getNom());
+            pst.setInt(2, entity.getAnneeFondation());
+            pst.setInt(3, entity.getId());
+
+            int rows = pst.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Equipe modifié");
+            }
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void delete(Equipe entity) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String requet = "DELETE FROM equipe WHERE id_equipe = ?";
+        try (PreparedStatement pst = MyConnection.getInstance().getCnx().prepareStatement(requet)) {
+            pst.setInt(1, entity.getId());
+
+            int rows = pst.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Equipe supprimé");
+            }
+        } catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
     }
     
 }
