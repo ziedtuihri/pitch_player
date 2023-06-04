a40e2ef8d9da9580f93cd9a4227b8269b3637386
zied <zied.hack@gmail.com>
2023-06-04 14:29:26 +0200

    login interface

---
+++ src/edu/connection1cinfo1/gui/LoginFXMLController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package edu.connection1cinfo1.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author ztouahri2
+ */
+public class LoginFXMLController implements Initializable {
+
+    @FXML
+    private TextField idUsername;
+    @FXML
+    private TextField idPassword;
+    @FXML
+    private Text idMessage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void login(ActionEvent event) {
+        
+        
+        
+    }
+    
+}
