3e2a9a792412d149c3dcaafbff5e93230e07adf7
zied <zied.hack@gmail.com>
2023-06-25 23:43:46 +0200

    css FXML notifications

---
+++ src/edu/connection1cinfo1/services/NotificationCRUD.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.connection1cinfo1.services;
+
+import edu.connection1cinfo1.entities.Notification;
+import edu.connection1cinfo1.interfaces.ICRUD;
+import java.util.List;
+
+/**
+ *
+ * @author ztouahri2
+ */
+public class NotificationCRUD implements ICRUD<Notification> {
+
+    @Override
+    public void addEntity(Notification t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Notification> displayEntities() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Notification getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(Notification entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Notification entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
